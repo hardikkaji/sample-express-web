0c28e94c2b53c601f924153dbfa7a9db4c5d92a9
Hardik Kaji <hardik.kaji@mastercard.com>
2017-03-28 15:57:55 +0530

    comments added;

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+
+// 1. Import Express
 var express = require('express');
+
 var customers = require('./customers.json');
+
+// 2. Load Express
 var app = express();
 var path = require('path');
 
+// 3. Map Request and Response
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+	res.send("Hello World");
+	//res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.use(express.static('node_modules'));
@@ -13,6 +20,7 @@ app.get('/api/customers', function(req, res) {
 	res.send(customers);
 });
 
+// 4. Bind server with Port
 app.listen(3000, function() {
 	console.log('Express started on localhost:3000');
 });
